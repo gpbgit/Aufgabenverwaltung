d99b074e0a2f66f5ca0e022ea6f47c84a827d272
charleth <C.Gruben@gmx.de>
2021-06-02 17:31:34 +0200

    Stand v. Mouton-Sourcecode 'Einstiegshilfe'

--- Aufgabenliste/Program.cs
+++ Aufgabenliste/Program.cs
@@ -16,7 +16,7 @@ namespace Aufgabenliste
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new AufgabenverwaltungMain());
+            Application.Run(new Aufgabenverwaltung());
         }
     }
 }
